abc0d83c555bd23ba9deeb3e9e963d36ac9be08f
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-18 01:08:10 +0100

    Style correction on flag_getter file

--- flag_getter.c
+++ flag_getter.c
@@ -8,29 +8,29 @@
  */
 int flag_getter(const char *format, int *i)
 {
-    /* Flags: '-' '+' '0' '#' ' ' */
-    /* Corresponding values: 1 2 4 8 16 */
-    int j, curr_i;
-    int flg = 0;
-    const char CHARACTER_FLAGS[] = {'-', '+', '0', '#', ' ', '\0'};
-    const int FLAGS_VALUES[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	/* Flags: '-' '+' '0' '#' ' ' */
+	/* Corresponding values: 1 2 4 8 16 */
+	int j, curr_i;
+	int flg = 0;
+	const char CHARACTER_FLAGS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_VALUES[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-    {
-        for (j = 0; CHARACTER_FLAGS[j] != '\0'; j++)
-        {
-            if (format[curr_i] == CHARACTER_FLAGS[j])
-            {
-                flg |= FLAGS_VALUES[j];
-                break;
-            }
-        }
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; CHARACTER_FLAGS[j] != '\0'; j++)
+		{
+			if (format[curr_i] == CHARACTER_FLAGS[j])
+			{
+				flg |= FLAGS_VALUES[j];
+				break;
+			}
+		}
 
-        if (CHARACTER_FLAGS[j] == 0)
-            break;
-    }
+		if (CHARACTER_FLAGS[j] == 0)
+			break;
+	}
 
-    *i = curr_i - 1;
+	*i = curr_i - 1;
 
-    return flg;
+	return (flg);
 }
